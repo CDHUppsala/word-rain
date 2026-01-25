74ed8240b1b91a267ecdde51ab6cdf7e54e9f0eb
Maria Skeppstedt <mask3308@LIN-MASK3308-01.ling.su.se>
2026-01-25 14:23:46 +0100

    How to create a conda environment and install packages

---
+++ conda.sh
@@ -0,0 +1,8 @@
+conda create --name word_rain python=3.12
+conda install anaconda::numpy
+conda install anaconda::scikit-learn
+conda install anaconda::gensim
+conda install anaconda::nltk
+conda install anaconda::matplotlib
+conda install anaconda::python-bidi
+conda install anaconda::reportlab
